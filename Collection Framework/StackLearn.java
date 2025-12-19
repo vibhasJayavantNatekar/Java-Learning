d815839f1a8996a85e2b14dbe0c0100df394072b
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-12-19 19:30:47 +0530

    Learn  about  linkedList  in collection interface

---
+++ Collection Framework/StackLearn.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class StackLearn {
+    public static void main(String[] args) {
+        Stack<String> animals = new Stack<>();
+
+        animals.push("Lion");
+        animals.push("Dog");
+         animals.push("Horse");
+        animals.push("Cat");
+       
+
+        System.out.println("Stack :" + animals);
+
+        System.out.println(animals.peek());
+
+
+        animals.pop();
+        System.out.println("Stack :" + animals);
+
+        System.out.println(animals.peek());
+
+
+    }
+    
+}
